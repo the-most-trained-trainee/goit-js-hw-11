fa316fc2e7d14758782f41ded641d56592724e59
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-07 00:46:05 +0300

    Interim Commit 07082022

--- src/index.js
+++ src/index.js
@@ -1,39 +1,42 @@
-import axios from "axios";
+import FetchPhotos from "./fetchPhotos.js";
 
 const searchQueryForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-form > input");
+const galleryPart = document.querySelector(".gallery");
+const loadMoreButton = document.querySelector(".load-more");
+
+const fetchMaker = new FetchPhotos();
 
 searchQueryForm.addEventListener("submit", onSubmit);
 
+loadMoreButton.addEventListener("click", fetchArticles);
+
 function onSubmit(event) {
-    event.preventDefault();
-    const enteredRequest = searchInput.value;
-
-    // fetch("https://pixabay.com/api/?key=29078045-8c2db167d821a84d590b709ce&q=yellow+flowers&image_type=photo&pretty=true")
-    //     .then((response) => {
-    //         return response.json();
-    //     })
-    //     .then((data) => {
-    //         console.log(data);
-    //     })
-    axios.get("https://pixabay.com/api/?key=29078045-8c2db167d821a84d590b709ce&q=yellow+flowers&image_type=photo&pretty=true")
-        .then((response) => {
-            console.log(response);
-        })
-    // .then((data) => {
-    //     console.log(data);
-    // })
-
-
-
-
-    event.currentTarget.reset();
+  event.preventDefault();
+  galleryPart.innerHTML = "";
+  fetchMaker.resetPage();
+  fetchMaker.request = searchInput.value;
+  fetchArticles();
+  event.currentTarget.reset();
 }
 
+function fetchArticles() {
+  fetchMaker.newFetch().then(res => cardBuilder(res.data.hits));
+}
 
-
-// key - твій унікальний ключ доступу до API.
-//     q - термін для пошуку.Те, що буде вводити користувач.
-//         image_type - тип зображення.На потрібні тільки фотографії, тому постав значення photo.
-//             orientation - орієнтація фотографії.Постав значення horizontal.
-//                 safesearch - фільтр за віком.Постав значення true.
+function cardBuilder(photoSet) {
+  let galleryToInput = "";
+  photoSet.forEach(photo => {
+    galleryToInput = galleryToInput + `
+        <div class="photo-card">
+            <img src="${photo.webformatURL}" alt="" loading="lazy" />
+            <div class="info">
+                <p class="info-item"><b>Likes</b> ${photo.likes}</p>
+                <p class="info-item"><b>Views</b> ${photo.views}</p>
+                <p class="info-item"><b>Comments</b> ${photo.comments}</p>
+                <p class="info-item"><b>Downloads</b> ${photo.downloads}</p>
+            </div>
+        </div>`
+  });
+  galleryPart.innerHTML += galleryToInput;
+}
