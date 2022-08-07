9e92ddaa172d65b48aed8da9ab4820ba65397e28
the-most-trained-trainee <oleg.lavrenko@outlook.com>
2022-08-07 14:26:45 +0300

    Interim Commit (First Working Version) 07082022

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -5,13 +5,13 @@ export default class FetchPhotos {
   constructor() {
     this.searchRequest = "";
     this.pageNumber = 1;
+    this.cardsNumber = 40;
   }
 
   newFetch = () => {
     const PIXABAY_KEY = "29078045-8c2db167d821a84d590b709ce"
     const defaultApiSettings = "image_type=photo&orientation=horizontal&safesearch=true"
-    const cardsNumber = 4;
-    const result = axios.get(`https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${this.searchRequest}&${defaultApiSettings}&page=${this.pageNumber}&per_page=${cardsNumber}`);
+    const result = axios.get(`https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${this.searchRequest}&${defaultApiSettings}&page=${this.pageNumber}&per_page=${this.cardsNumber}`);
     result.then(() => this.pageNumber++);
     return result;
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import FetchPhotos from "./fetchPhotos.js";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchQueryForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#search-form > input");
 const galleryPart = document.querySelector(".gallery");
 const loadMoreButton = document.querySelector(".load-more");
+loadMoreButton.style.display = "none";
 
 const fetchMaker = new FetchPhotos();
 
 searchQueryForm.addEventListener("submit", onSubmit);
-
 loadMoreButton.addEventListener("click", fetchArticles);
 
 function onSubmit(event) {
@@ -21,22 +24,50 @@ function onSubmit(event) {
 }
 
 function fetchArticles() {
-  fetchMaker.newFetch().then(res => cardBuilder(res.data.hits));
+  fetchMaker.newFetch().then(res => cardBuilder(res.data));
 }
 
 function cardBuilder(photoSet) {
+  photosCounter(photoSet);
   let galleryToInput = "";
-  photoSet.forEach(photo => {
+  photoSet.hits.forEach(photo => {
     galleryToInput = galleryToInput + `
-        <div class="photo-card">
-            <img src="${photo.webformatURL}" alt="" loading="lazy" />
-            <div class="info">
-                <p class="info-item"><b>Likes</b> ${photo.likes}</p>
-                <p class="info-item"><b>Views</b> ${photo.views}</p>
-                <p class="info-item"><b>Comments</b> ${photo.comments}</p>
-                <p class="info-item"><b>Downloads</b> ${photo.downloads}</p>
-            </div>
-        </div>`
+    <div class="photo-card">
+      <div>
+        <a href="${photo.largeImageURL}" class="gallery__link">
+          <img src="${photo.webformatURL}" alt="" loading="lazy" class="gallery__image" />    
+        </a>
+      </div>
+      <div class="info">
+        <p class="info-item"><b>Likes</b><br>${photo.likes}</p>
+        <p class="info-item"><b>Views</b><br>${photo.views}</p>
+        <p class="info-item"><b>Comments</b><br>${photo.comments}</p>
+        <p class="info-item"><b>Downloads</b><br>${photo.downloads}</p>
+      </div>
+    </div>`
   });
   galleryPart.innerHTML += galleryToInput;
+  let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+}
+
+function photosCounter(initialData) {
+  const totalHits = initialData.totalHits;
+  const currentPage = fetchMaker.pageNumber - 1;
+  const cardsNumber = fetchMaker.cardsNumber;
+  const maxPageNumber = totalHits / cardsNumber;
+
+  if (currentPage === 1 && maxPageNumber > 1) {
+    loadMoreButton.style.display = "block";
+  }
+
+  if (currentPage === 1 && totalHits > 0) {
+    Notify.success(`Hooray! We found ${totalHits} images.`)
+  } else if (currentPage === 1 && totalHits === 0) {
+    Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+  }
+
+  if (currentPage >= maxPageNumber && totalHits > cardsNumber) {
+    loadMoreButton.style.display = "none";
+    Notify.info("We're sorry, but you've reached the end of search results.")
+  }
 }
